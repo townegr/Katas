c1a6fe10732ddaf7b0aa0a9b1cd0466bda359cc6
Greg Towne <townegr@yahoo.com>
2014-05-03 16:32:49 -0400

    max-value

--- max_number/lib/max_number.rb
+++ max_number/lib/max_number.rb
@@ -1,3 +1,15 @@
-def max_number(numbers)
-  # YOUR CODE GOES HERE
+numbers = [12, 2, 33, 24, 15]
+max = numbers[0]
+
+#assigning largest value to max variable through iteration
+numbers.each do |n|
+  if n >= max
+    max = n
+  end
 end
+
+p max
+
+#using inject method to compare elements in array
+p max = numbers.inject{ |max, n| n > max ? n : max }
+
