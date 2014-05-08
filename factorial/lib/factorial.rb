418c72ef16f3b4034fb9e27d90d7c446be043401
Greg Towne <townegr@yahoo.com>
2014-05-08 11:41:36 -0400

    cached fib

--- factorial/lib/factorial.rb
+++ factorial/lib/factorial.rb
@@ -11,3 +11,18 @@ def factorial(n)
     n * factorial(n-1)
   end
 end
+
+#cached fibonacci
+cache = Hash.new
+
+def fib_cache(n)
+     if t = cache[n]
+          return t
+     end
+     if n <=1
+     return n
+     else
+     result = fib_cache(n-1) + fib_cache(n-2)
+     end
+     cache[n] = result
+end
